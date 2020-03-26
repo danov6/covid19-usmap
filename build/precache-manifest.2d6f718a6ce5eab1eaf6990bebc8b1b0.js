78c2a0d63178e26dd711c3cb13931ff0ebc76b8f
Gio <danov6@aol.com>
2020-03-25 21:44:59 -0700

    Update

---
+++ build/precache-manifest.2d6f718a6ce5eab1eaf6990bebc8b1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80d0f01a3bcbaf2c5f369eca68ac6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9bd7aa549fe37f4ada",
+    "url": "/static/css/main.c94e2f08.chunk.css"
+  },
+  {
+    "revision": "83ec93c2f5867d215073",
+    "url": "/static/js/2.a2f06894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2f06894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9bd7aa549fe37f4ada",
+    "url": "/static/js/main.70fee4c9.chunk.js"
+  },
+  {
+    "revision": "9923edc35664cec778e6",
+    "url": "/static/js/runtime-main.88261b29.js"
+  }
+]);
