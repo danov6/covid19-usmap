ff04e7f2e08d5a1adb5a7bb835618884ff21b228
Gio <danov6@aol.com>
2020-04-04 17:15:39 -0700

    build files

---
+++ build/precache-manifest.ee48a545bf9b2bc5a58ebd90966d1037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ce55af14c53eea196b43bcb0cca908",
+    "url": "/covid19-usmap/index.html"
+  },
+  {
+    "revision": "c275057d51592c647c28",
+    "url": "/covid19-usmap/static/css/main.4345b386.chunk.css"
+  },
+  {
+    "revision": "6dfee5428812e5f827dc",
+    "url": "/covid19-usmap/static/js/2.2486b7aa.chunk.js"
+  },
+  {
+    "revision": "edef54ed98989d4dafa2bd79d8f6fefa",
+    "url": "/covid19-usmap/static/js/2.2486b7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c275057d51592c647c28",
+    "url": "/covid19-usmap/static/js/main.e61e530d.chunk.js"
+  },
+  {
+    "revision": "e0ae98de0ded05b99b7f",
+    "url": "/covid19-usmap/static/js/runtime-main.bc92c16f.js"
+  }
+]);
